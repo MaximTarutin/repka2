8b0d3e45f781c432fb251bb9c313359368849982
MaximTarutin <mtarut@mail.ru>
2025-02-22 20:26:41 +0300

    Добавлен файл ресурсов, определенно разрешение экрана

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -2,6 +2,49 @@
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
-{}
+{
+    background_logo = new QLabel(this);
+    background_logo->setStyleSheet("border-image: url(:/resource/logo/repka-fon.jpg);");
+    this->setCentralWidget(background_logo);
 
-MainWindow::~MainWindow() {}
+    button_start = new QPushButton(this);
+    button_start->setStyleSheet("border-image: url(:/resource/logo/prosrach.png);");
+    button_start->setStyleSheet("border-image: url(:/resource/logo/start.png);");
+    button_start->move(100,100);
+    button_start->resize(100,100);
+
+    button_exit = new QPushButton(this);
+    button_exit->setStyleSheet("border-image: url(:/resource/logo/prosrach.png);");
+    button_exit->setStyleSheet("border-image: url(:/resource/logo/exit2.png);");
+    button_exit->move(400,400);
+    button_exit->resize(100,100);
+
+    screen_size();
+
+    qDebug() << WIDTH_SCREEN << HEIGHT_SCREEN;
+
+    connect(button_exit, &QPushButton::clicked, this, &MainWindow::exit_of_game);
+}
+
+MainWindow::~MainWindow()
+{
+    delete button_exit;
+    delete button_start;
+    delete background_logo;
+}
+
+void MainWindow::exit_of_game()
+{
+    exit(0);
+}
+
+void MainWindow::screen_size()
+{
+    WIDTH_SCREEN  = QGuiApplication::primaryScreen()->geometry().width();
+    HEIGHT_SCREEN = QGuiApplication::primaryScreen()->geometry().height();
+}
+
+
+
+
+
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,10 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QLabel>
+#include <QPushButton>
+#include <QGuiApplication>
+#include <QScreen>
 
 class MainWindow : public QMainWindow
 {
@@ -10,5 +14,21 @@ class MainWindow : public QMainWindow
 public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
+
+private:
+    QLabel              *background_logo = nullptr;                     // Фон
+    QPushButton         *button_start = nullptr;                        // Кнопка старт
+    QPushButton         *button_exit = nullptr;                         // Кнопка выход
+
+    int WIDTH_SCREEN;                                               // Ширина экрана
+    int HEIGHT_SCREEN;                                              // Высота экрана
+
+    void screen_size();                                             // Получаем разрешение экрана и
+                                                                    // определяем результаты в переменные
+                                                                    // WIDTH_SCREEN и SCREEN_HEIGHT
+
+private slots:
+    void exit_of_game();                                            // Выход из игры
+
 };
 #endif // MAINWINDOW_H
